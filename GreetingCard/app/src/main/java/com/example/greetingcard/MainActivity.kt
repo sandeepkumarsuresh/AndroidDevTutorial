192235f67fd207a16ba1f67cb27f52cf4b6e48f9
sandeepkumarsuresh <sandeepsuresh619@gmail.com>
2024-07-19 11:16:14 +0530

    Remove nested repository from index

---
+++ GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.greetingcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.greetingcard.ui.theme.GreetingCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GreetingCardTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable // Composable function does not return anything
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Red) {
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = modifier
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    GreetingCardTheme {
+//        Greeting("Android")
+//    }
+//}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview(){
+    GreetingCardTheme {
+        Greeting(name = "Sandeep")
+    }
+}
